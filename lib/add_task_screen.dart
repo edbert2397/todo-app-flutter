5d0472511f5a4967dc29df5c4860b5f6b6bb110c
edbert2397 <edberthalim29@gmail.com>
2024-02-18 17:27:27 +0700

    create list of daily task dummy for homepage and the temporary ui

---
+++ lib/add_task_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class addTask extends StatelessWidget {
+  const addTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:todoapp/add_task_screen.dart';
 import 'package:todoapp/bottom_navbar.dart';
 
 class homeScreen extends StatefulWidget {
@@ -10,57 +11,128 @@ class homeScreen extends StatefulWidget {
 }
 
 class _homeScreenState extends State<homeScreen> {
+  List<String> items = [
+    'Work Out',
+    'Daily Meeting',
+    'Reading a Book',
+    'Daily Meeting',
+    'Assignment Revision',
+    "fadff",
+    "faaf",
+    "123",
+    "2234",
+    "4ef"
+  ];
+  late List<bool> isSelected;
+
+  @override
+  void initState() {
+    super.initState();
+    isSelected = List.generate(items.length,(_) => false);
+  }
   @override
   Widget build(BuildContext context) {
     return 
     Scaffold(
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 30,vertical: 50),        
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30,vertical: 50),  
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            timeDisplay(),
-            SizedBox(height: 30),
-            Text("Welcome Edbert.H",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-
-              ),
-            ),
-            Text("Have a nice day !",
-              style: TextStyle(
-                fontSize: 25,
-              ),
-            ),
-            SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children:[
-                Text("Daily Task",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              timeDisplay(),
+              SizedBox(height: 30),
+              Text("Welcome Edbert.H",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+        
                 ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Color(0xff5038BC)),
-                  ),
-                  onPressed: () {},
-                  child: const Text('Add Task',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-  
+              ),
+              Text("Have a nice day !",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:[
+                  Text("Daily Task",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
                     ),
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Color(0xff5038BC)),
+                    ),
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => addTask()));
+                    },
+                    child: const Text('Add Task',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+          
+                      ),
+                  ),
+                ]
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (context,index){
+                    return GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          isSelected[index] = !isSelected[index];
+                        });
+                      },
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        padding: EdgeInsets.symmetric(horizontal:20,vertical: 15),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(items[index],
+                              style:TextStyle(
+                                color: isSelected[index]? Color(0xff5038BC) : Colors.black
+
+
+                              ),
+                            ),
+                      
+                            Container(
+                              height: 20,
+                              width: 20,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: Color(0xff5038BC),
+                                  width: 2,
+                                
+                                ),
+                                color: isSelected[index]? Color(0xff5038BC) : Colors.transparent
+                              ),
+                            )
+                          ],
+                        )
+                      
+                      ),
+                    );
+                  }
+                
                 ),
-              ]
-            )
-          ],
-        ),
+              )
+            ],
+          ),
       ),
-      bottomNavigationBar: bottomNavbar(),
+        bottomNavigationBar: bottomNavbar(),
     );
   }
 }
